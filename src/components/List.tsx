d1facd42bdc2ce422a49e3bdf57993b63869f296
Andre <andrevitorlopes@gmail.com>
2022-11-06 15:56:43 +0100

    Add items

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,9 +1,17 @@
-import { ReactElement, useEffect, useState } from 'react';
+import { Button, FormControl, TextField } from '@mui/material';
+import {
+  ChangeEvent,
+  FormEvent,
+  ReactElement,
+  useEffect,
+  useState,
+} from 'react';
 import { Item } from './Item';
 import SuperListItem from './SuperListItem/SuperListItem';
 
 const ItemsList = (): ReactElement => {
   const [items, setItems] = useState<Item[]>([]);
+  const [newItemName, setNewItem] = useState<string>('');
 
   useEffect(() => {
     const myItems: Item[] = [1, 2, 3, 4].map((i) => {
@@ -19,26 +27,81 @@ const ItemsList = (): ReactElement => {
 
   const onClickItem = (item: Item) => {
     const updatedItems = [...items];
-    const itemIndex = updatedItems.findIndex(i => i.id === item.id);
+    const itemIndex = updatedItems.findIndex((i) => i.id === item.id);
 
     updatedItems[itemIndex] = {
       ...items[itemIndex],
-      done: !item.done
+      done: !item.done,
     };
 
     setItems(updatedItems);
   };
 
-  const list = items.map((i) => (
+  const onUpdateNewItem = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+
+    setNewItem(value);
+  };
+
+  const onAddItem = (event: FormEvent) => {
+    event.preventDefault();
+
+    const updatedItems = [...items];
+    const newItem: Item = {
+      done: false,
+      name: newItemName,
+      id: (Math.random() * 1000000).toString(),
+    };
+
+    updatedItems.push(newItem);
+
+    setItems(updatedItems);
+  };
+
+  const list: ReactElement[] = items.map((i) => (
     <SuperListItem
       key={i.id}
       done={i.done}
-      name={`Item ${i.id}`}
+      name={i.name}
       onItemClick={() => onClickItem(i)}
     />
   ));
 
-  return <>{list}</>;
+  return (
+    <>
+      <form onSubmit={onAddItem}>
+        <FormControl
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            flexDirection: 'row',
+            justifyContent: 'flex-start',
+          }}
+        >
+          <TextField
+            required
+            name="newItem"
+            id="standard-required"
+            label="new item"
+            value={newItemName}
+            margin="normal"
+            onChange={onUpdateNewItem}
+          />
+          <Button
+            style={{ margin: '5px', height: '100%' }}
+            size="small"
+            type="submit"
+            variant="contained"
+            color="primary"
+          >
+            Add
+          </Button>
+        </FormControl>
+      </form>
+      <hr />
+      {list}
+    </>
+  );
 };
 
 export default ItemsList;
